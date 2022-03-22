ae0d1c7bda0f32838b6e39c88d130a843dbb8836
Rohit-Gupta-IIC <rohit.gupta@iic.ac.in>
2022-03-22 11:08:45 +0530

    22 March 2022

--- contracts_01/counter/step_01.py
+++ contracts_01/counter/step_01.py
@@ -5,13 +5,29 @@ def approval():
     global_owner = Bytes("owner")
     global_counter = Bytes("counter")
 
+    op_increment = Bytes("inc")
+    op_decrement = Bytes("dec")
+
+    increment = Seq(
+        App.globalPut(global_counter,App.globalGet(global_counter) + Int(1)),
+        Approve()
+    )
+
+    decrement = Seq(
+        App.globalPut(global_counter,App.globalGet(global_counter) - Int(1)),
+        Approve()
+    )
+
     return program.event(
         init=Seq(
             App.globalPut(global_counter, Int(0)),
             App.globalPut(global_owner, Txn.sender()),
             Approve()
         ),
-        # no_op=,
+        no_op=Cond(
+            [Txn.application_args[0]==op_increment, increment],
+            [Txn.application_args[0]==op_decrement, decrement],
+        ),
     )
 
 def clear():
